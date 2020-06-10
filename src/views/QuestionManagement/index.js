e92ea7967e25211a11da582f4015eef1fff70467
anandmohansinha <anandsinhaknit@gmail.com>
2020-06-10 16:33:55 +0400

    code changes for QuestionManagement

--- src/views/QuestionManagement/index.js
+++ src/views/QuestionManagement/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React, { Component, useEffect } from 'react'
 // core components
 import GridItem from "components/Grid/GridItem.js";
 import GridContainer from "components/Grid/GridContainer.js";
@@ -15,18 +15,67 @@ import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
 
 export default function QuestionManagement () {
-    const [value, setValue] = React.useState('female');
+    const [questions, setQuestions] = React.useState([]);
+    const[selectedIndex, setSelectedIndex]=React.useState(0);
+    const[count, setCount]=React.useState(1);
+    const[technology, setTechnology]=React.useState('');
+    const[ nextDisable, setNextDisable] = React.useState(false);
+    const[ preDisable, setPrevDisable] = React.useState(true);
 
-    const handleChange = (event) => {
-        setValue(event.target.value);
+    const next = (event) => {
+        debugger
+        if(questions.length>0 && questions.length==count+1){
+            setNextDisable(true);
+        }
+            setCount(count+1);
+            setPrevDisable(false);
+            setSelectedIndex(selectedIndex+1);
+        
     };
+    const prev = (event) => {
+        if(questions.length>0 && count==2){
+            setPrevDisable(true);
+        }
+        setCount(count-1)
+        setNextDisable(false);
+        setSelectedIndex(selectedIndex-1); 
+    };
+    const handleChange = (event)=>{
+        
+    }
+
+useEffect(()=>callApi(), []);
+
+const callApi=()=>{
+    debugger
+    fetch('http://18.223.111.230:8080/assessment/3?emailId=KUMAR.ABHISHEK1@synechron.com', {
+        method:'GET',
+        headers:{
+          'Accept':'application/json',
+          'Content-Type':'application/json',
+          'Access-Control-Allow-Origin': 'Origin, X-Requested-With, Content-Type, Accept'
+        }
+      }).then((res)=>res.json())
+        .then((res) =>{
+            debugger
+            setQuestions(res.assessments.questions);
+            setTechnology(res.assessments.technology)
+        })
+
+}
+
+if(questions.length == 0 || questions[selectedIndex]=='undefined'){
+    debugger
+    return "";
+}
     return (
+
         <GridContainer>
             <GridItem xs={12} sm={12} md={12}>
                 <Card>
                     <CardHeader color="primary">
                         <Typography variant="h6">
-                            Technology - JAVA
+                            Technology - {technology}
                         </Typography>
                         {/* <p className={classes.cardCategoryWhite}>
               Here is a subtitle for this table
@@ -36,28 +85,23 @@ export default function QuestionManagement () {
                     <CardBody>
                         <div>
                             <Typography variant="h6" mt={8}>
-                                Sample question relevant to technology?
+                              {count}. {questions[selectedIndex].header}
                             </Typography>
                             <FormControl component="fieldset">
-                                <FormLabel component="legend">Gender</FormLabel>
-                                <RadioGroup aria-label="gender" name="gender1" value={value} onChange={handleChange}>
-                                    <FormControlLabel value="female" control={<Radio />} label="Female" />
-                                    <FormControlLabel value="male" control={<Radio />} label="Male" />
-                                    <FormControlLabel value="other" control={<Radio />} label="Other" />
-                                    <FormControlLabel value="disabled" disabled control={<Radio />} label="(Disabled option)" />
-                                </RadioGroup>
+                            <RadioGroup aria-label="gender" name="gender1" value="" onChange={handleChange}>
+                            {
+                                questions[selectedIndex].options.map((option,index) =>(
+                                    <FormControlLabel value={option.description} control={<Radio />} label={option.description} />
+                                   
+                                ))
+                           } 
+                            </RadioGroup>
+
+                          
+                                
                             </FormControl>
                         </div>
                     </CardBody>
-
-                    <Button
-                        type="button"
-                        variant="contained"
-                        color="primary"
-                        size="large"
-                    >
-                        Modify 
-                  </Button>
                 </Card>
                 <div>
                     <Button
@@ -65,8 +109,20 @@ export default function QuestionManagement () {
                         variant="contained"
                         color="secondary"
                         size="large"
+                        disabled={preDisable}
+                        onClick={()=>prev()}
                     >
-                        + ADD
+                        Prev
+                  </Button>
+                  <Button
+                        type="button"
+                        variant="contained"
+                        color="secondary"
+                        size="large"
+                        disabled={nextDisable}
+                        onClick={()=>next()}
+                    >
+                        Next
                   </Button>
                 </div>
             </GridItem>
