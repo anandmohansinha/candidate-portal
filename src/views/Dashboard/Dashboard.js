456e5d84029b7bcac44fc7c0413086f752f7dcd4
anandmohansinha <anandsinhaknit@gmail.com>
2020-06-15 12:49:10 +0400

    code commit for dashboard

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React ,{useEffect} from "react";
 // react plugin for creating charts
 import ChartistGraph from "react-chartist";
 // @material-ui/core
@@ -28,7 +28,7 @@ import CardHeader from "components/Card/CardHeader.js";
 import CardIcon from "components/Card/CardIcon.js";
 import CardBody from "components/Card/CardBody.js";
 import CardFooter from "components/Card/CardFooter.js";
-import Button from '@material-ui/core/Button';
+import javaLogo from "../../assets/img/java.jpg";
 
 import { bugs, website, server } from "variables/general.js";
 
@@ -42,40 +42,187 @@ import styles from "assets/jss/material-dashboard-react/views/dashboardStyle.js"
 
 const useStyles = makeStyles(styles);
 
+export const JavaLogo = () => (
+  <Icon>
+      <img src={javaLogo} height={50} width={50}/>
+  </Icon>
+)
+
 export default function Dashboard() {
+  const[subjectCount, setSubjectCount]= React.useState([]);
   const classes = useStyles();
+  useEffect(()=>callApi(), []);
+
+  const callApi=()=>{
+    fetch('http://18.223.111.230:8080/candidateAssessmentCount', {
+        method:'GET',
+        headers:{
+          'Accept':'application/json',
+          'Content-Type':'application/json',
+          'Access-Control-Allow-Origin': 'Origin, X-Requested-With, Content-Type, Accept'
+        }
+      }).then((res)=>res.json())
+        .then((res) =>{
+          setSubjectCount(res);
+        })
+  }
+  if(subjectCount.length === 0 ){
+    return null
+  }
   return (
     <div>
-      {/* <GridContainer>
+      <GridContainer>
+        <GridItem xs={12} sm={6} md={3}>
+          <Card>
+            <CardHeader color="warning" stats icon>
+              <CardIcon color="warning">
+                <JavaLogo></JavaLogo>
+              </CardIcon>
+              <p className={classes.cardCategory}>{subjectCount[2].assessmentName}</p>
+              <h3 className={classes.cardTitle}>
+              {subjectCount[2].candidateCount} <small></small>
+              </h3>
+            </CardHeader>
+            <CardFooter stats>
+              <div className={classes.stats}>
+                <Danger>
+                  <Warning />
+                </Danger>
+                <a href="#pablo" onClick={e => e.preventDefault()}>
+                  Java Candidate
+                </a>
+              </div>
+            </CardFooter>
+          </Card>
+        </GridItem>
+        <GridItem xs={12} sm={6} md={3}>
+          <Card>
+            <CardHeader color="success" stats icon>
+              <CardIcon color="success">
+                <Store />
+              </CardIcon>
+              <p className={classes.cardCategory}>{subjectCount[1].assessmentName}</p>
+              <h3 className={classes.cardTitle}> {subjectCount[1].candidateCount}</h3>
+            </CardHeader>
+            <CardFooter stats>
+              <div className={classes.stats}>
+                <DateRange />
+               Angular candidate
+              </div>
+            </CardFooter>
+          </Card>
+        </GridItem>
+        <GridItem xs={12} sm={6} md={3}>
+          <Card>
+            <CardHeader color="danger" stats icon>
+              <CardIcon color="danger">
+                <Icon>info_outline</Icon>
+              </CardIcon>
+              <p className={classes.cardCategory}>{subjectCount[3].assessmentName}</p>
+              <h3 className={classes.cardTitle}> {subjectCount[3].candidateCount}</h3>
+            </CardHeader>
+            <CardFooter stats>
+              <div className={classes.stats}>
+                <LocalOffer />
+                React candidate
+              </div>
+            </CardFooter>
+          </Card>
+        </GridItem>
+        <GridItem xs={12} sm={6} md={3}>
+          <Card>
+            <CardHeader color="info" stats icon>
+              <CardIcon color="info">
+                <Accessibility />
+              </CardIcon>
+              <p className={classes.cardCategory}>{subjectCount[4].assessmentName}</p>
+              <h3 className={classes.cardTitle}> {subjectCount[4].candidateCount}</h3>
+            </CardHeader>
+            <CardFooter stats>
+              <div className={classes.stats}>
+                <Update />
+               SpringBoot candidate
+              </div>
+            </CardFooter>
+          </Card>
+        </GridItem>
+       </GridContainer>
+     {/* <GridContainer>
         <GridItem xs={12} sm={12} md={4}>
           <Card chart>
+            <CardHeader color="success">
+              <ChartistGraph
+                className="ct-chart"
+                data={dailySalesChart.data}
+                type="Line"
+                options={dailySalesChart.options}
+                listener={dailySalesChart.animation}
+              />
+            </CardHeader>
             <CardBody>
-              <h4 className={classes.cardTitle}>Invite Candidates for Assessment</h4>
-              <p className={classes.cardCategory}>Invitation</p>
+              <h4 className={classes.cardTitle}>Daily Sales</h4>
+              <p className={classes.cardCategory}>
+                <span className={classes.successText}>
+                  <ArrowUpward className={classes.upArrowCardCategory} /> 55%
+                </span>{" "}
+                increase in today sales.
+              </p>
             </CardBody>
             <CardFooter chart>
-               
-                <Button size="large" variant="outlined" color="primary">
-                  Invite
-                </Button>
+              <div className={classes.stats}>
+                <AccessTime /> updated 4 minutes ago
+              </div>
             </CardFooter>
           </Card>
         </GridItem>
         <GridItem xs={12} sm={12} md={4}>
           <Card chart>
+            <CardHeader color="warning">
+              <ChartistGraph
+                className="ct-chart"
+                data={emailsSubscriptionChart.data}
+                type="Bar"
+                options={emailsSubscriptionChart.options}
+                responsiveOptions={emailsSubscriptionChart.responsiveOptions}
+                listener={emailsSubscriptionChart.animation}
+              />
+            </CardHeader>
             <CardBody>
-              <h4 className={classes.cardTitle}>Question Management</h4>
-              <p className={classes.cardCategory}>Add/Remove Questions</p>
+              <h4 className={classes.cardTitle}>Email Subscriptions</h4>
+              <p className={classes.cardCategory}>Last Campaign Performance</p>
             </CardBody>
             <CardFooter chart>
-                <Button size="large" variant="outlined" color="primary">
-                  Manage
-                </Button>
+              <div className={classes.stats}>
+                <AccessTime /> campaign sent 2 days ago
+              </div>
             </CardFooter>
           </Card>
         </GridItem>
-      </GridContainer> */}
-
+        <GridItem xs={12} sm={12} md={4}>
+          <Card chart>
+            <CardHeader color="danger">
+              <ChartistGraph
+                className="ct-chart"
+                data={completedTasksChart.data}
+                type="Line"
+                options={completedTasksChart.options}
+                listener={completedTasksChart.animation}
+              />
+            </CardHeader>
+            <CardBody>
+              <h4 className={classes.cardTitle}>Completed Tasks</h4>
+              <p className={classes.cardCategory}>Last Campaign Performance</p>
+            </CardBody>
+            <CardFooter chart>
+              <div className={classes.stats}>
+                <AccessTime /> campaign sent 2 days ago
+              </div>
+            </CardFooter>
+          </Card>
+        </GridItem>
+      </GridContainer>  */}
+     
+        
     </div>
   );
 }
