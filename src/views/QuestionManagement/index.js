a50b0896a8575e971d6006477e50142cbe3eea5a
Herin <herin0506@gmail.com>
2020-06-18 22:00:33 +0530

    CSS issue related to Assignment fixed. The Alert code is commented with reason mentioned there.

--- src/views/QuestionManagement/index.js
+++ src/views/QuestionManagement/index.js
@@ -13,180 +13,181 @@ import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
+import styles from './QuestionManagement.module.css'
 
-export default function QuestionManagement (props) {
+export default function QuestionManagement(props) {
     const [questions, setQuestions] = React.useState([]);
-    const[selectedIndex, setSelectedIndex]=React.useState(0);
-    const[count, setCount]=React.useState(1);
-    const[technology, setTechnology]=React.useState('');
-    const[ nextDisable, setNextDisable] = React.useState(false);
-    const[ preDisable, setPrevDisable] = React.useState(true);
+    const [selectedIndex, setSelectedIndex] = React.useState(0);
+    const [count, setCount] = React.useState(1);
+    const [technology, setTechnology] = React.useState('');
+    const [nextDisable, setNextDisable] = React.useState(false);
+    const [preDisable, setPrevDisable] = React.useState(true);
     const [value, setValue] = React.useState('');
-    const[answers, setAnswer] = React.useState({});
-    const[email, setEmail]= React.useState();
+    const [answers, setAnswer] = React.useState({});
+    const [email, setEmail] = React.useState();
     const [isDataLoaded, setisDataLoaded] = React.useState(false);
     const [isTestSubmitted, setIsTestSubmitted] = React.useState(false);
-    let styleButtons     = {
-        marginRight: '5px',
-      };
 
     const next = (event) => {
-        if(questions.length>0 && questions.length==count+1){
+        if (questions.length > 0 && questions.length == count + 1) {
             setNextDisable(true);
         }
-            setCount(count+1);
-            setPrevDisable(false);
-            setSelectedIndex(selectedIndex+1);
+        setCount(count + 1);
+        setPrevDisable(false);
+        setSelectedIndex(selectedIndex + 1);
+        if (answers && answers.questionAnswerReq[selectedIndex + 1] && answers.questionAnswerReq[selectedIndex + 1].optionId) {
+            setValue(answers.questionAnswerReq[selectedIndex + 1].optionId);
+        }
     };
     const prev = (event) => {
-        if(questions.length>0 && count==2){
+        if (questions.length > 0 && count == 2) {
             setPrevDisable(true);
         }
-        setCount(count-1)
+        setCount(count - 1)
         setNextDisable(false);
-        setSelectedIndex(selectedIndex-1); 
+        setSelectedIndex(selectedIndex - 1);
+        setValue(answers.questionAnswerReq[selectedIndex - 1].optionId);
     };
-    const submit = ()=>{
-        fetch('http://52.14.195.214:8080/submitAssessment?emailId='+email, {
-        method:'POST',
-        headers:{
-          'Accept':'application/json',
-          'Content-Type':'application/json',
-          'Access-Control-Allow-Origin': 'Origin, X-Requested-With, Content-Type, Accept'
-        },
-        body: JSON.stringify(answers)
-      }).then((res)=>res.json())
-        .then((res) =>{
-            console.log("submitAssessment",res);
-            if(res.dataSubmited){
-                setIsTestSubmitted(true);
-            }
-            
-        })
+    const submit = () => {
+        fetch('http://52.14.195.214:8080/submitAssessment?emailId=' + email, {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': 'Origin, X-Requested-With, Content-Type, Accept'
+            },
+            body: JSON.stringify(answers)
+        }).then((res) => res.json())
+            .then((res) => {
+                if (res.dataSubmited) {
+                    setIsTestSubmitted(true);
+                }
+            })
     }
     const handleChange = (event) => {
         setValue(event.target.value);
-        let {questionAnswerReq} = answers;
+        let { questionAnswerReq } = answers;
         let quesAns = {
             "questionId": count,
             "optionId": event.target.value
         }
-       questionAnswerReq.push(quesAns);
-       
-      };
-
-    
-
-useEffect(()=>callApi(), []);
-
-const callApi=()=>{
-    fetch('http://52.14.195.214:8080/assessment/'+props.history.location.search, {
-        method:'GET',
-        headers:{
-          'Accept':'application/json',
-          'Content-Type':'application/json',
-          'Access-Control-Allow-Origin': 'Origin, X-Requested-With, Content-Type, Accept'
-        }
-      }).then((res)=>res.json())
-        .then((res) =>{
-            if(res.dataAvailable) {
-                setQuestions(res.assessments.questions);
-                setTechnology(res.assessments.technology);
-                setEmail(res.candidate.emailAddress);
-                setAnswer({"assessmentId": res.assessments.id, "questionAnswerReq": []});
+        questionAnswerReq.push(quesAns);
+        for (const key in questionAnswerReq) {
+            if (questionAnswerReq.hasOwnProperty(key)) {
+                const element = questionAnswerReq[key];
+                if (questionAnswerReq[key].questionId === quesAns.questionId && questionAnswerReq[key].optionId !== quesAns.optionId) {
+                    questionAnswerReq[key].optionId = quesAns.optionId;
+                    questionAnswerReq.pop();
+                }
             }
-            setisDataLoaded(true)
-        })
-}
+        }
+    };
 
-const radioButtonContent = ()=>{
-    return(
-        questions[selectedIndex].options.map((option,index) =>(
-            <FormControlLabel value={option.id} control={<Radio />} label={option.description} />
-        
-        ))
-    )
-}
+    useEffect(() => callApi(), []);
 
-// if(questions.length == 0 || questions[selectedIndex]=='undefined'){
-//     return "";
-// }
+    const callApi = () => {
+        fetch('http://52.14.195.214:8080/assessment/' + props.history.location.search, {
+            method: 'GET',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': 'Origin, X-Requested-With, Content-Type, Accept'
+            }
+        }).then((res) => res.json())
+            .then((res) => {
+                if (res.dataAvailable) {
+                    setQuestions(res.assessments.questions);
+                    setTechnology(res.assessments.technology);
+                    setEmail(res.candidate.emailAddress);
+                    setAnswer({ "assessmentId": res.assessments.id, "questionAnswerReq": [] });
+                }
+                setisDataLoaded(true)
+            })
+    }
+
+    const radioButtonContent = () => {
+        return (
+            questions[selectedIndex].options.map((option, index) => (
+                <FormControlLabel value={option.id} control={<Radio />} label={option.description} />
+
+            ))
+        )
+    }
+
+    // if(questions.length == 0 || questions[selectedIndex]=='undefined'){
+    //     return "";
+    // }
     return (
         <div>
-     <Alert severity="warning">This is a warning alert — check it out!</Alert>
-      <Alert severity="info">This is an info alert — check it out!</Alert>
-        {!isTestSubmitted && isDataLoaded && !questions.length && <label>There is not any active assignment assigned to you.Please contact recruiter.</label>}
-        { isTestSubmitted &&  <label>Your test has been submitted sucsessfully.We wish you good luck.If you are shortlisted our recruiter team will get in touch with you.Thanks.</label>}
-        {!isTestSubmitted &&  questions.length &&   <GridContainer>
-       <GridItem xs={12} sm={12} md={12}>
-                <Card>
-                    <CardHeader color="primary">
-                        <Typography variant="h6">
-                            Technology - {technology}
-                        </Typography>
-                        {/* <p className={classes.cardCategoryWhite}>
+            {/* Below alers are commented as it throws error as "Alert is not defined" */}
+            {/* <Alert severity="warning">This is a warning alert — check it out!</Alert>
+            <Alert severity="info">This is an info alert — check it out!</Alert> */}
+            {!isTestSubmitted && isDataLoaded && !questions.length && <label>There is not any active assignment assigned to you.Please contact recruiter.</label>}
+            {isTestSubmitted && <label>Your test has been submitted sucsessfully.We wish you good luck.If you are shortlisted our recruiter team will get in touch with you.Thanks.</label>}
+            {!isTestSubmitted && questions.length && <GridContainer>
+                <GridItem xs={12} sm={12} md={12}>
+                    <Card>
+                        <CardHeader color="primary">
+                            <Typography variant="h6">
+                                Technology - {technology}
+                            </Typography>
+                            {/* <p className={classes.cardCategoryWhite}>
               Here is a subtitle for this table
             </p> */}
 
-                    </CardHeader>
-                    <CardBody>
-                        <div>
-                            <Typography variant="h6" mt={8}>
-                              {count}. {questions[selectedIndex].header}
-                            </Typography>
-                            <FormControl component="fieldset">
-                             <FormLabel component="legend"></FormLabel>
-                                <RadioGroup aria-label="question" name="question" value={value} onChange={handleChange}>
-                                {
-                                    questions[selectedIndex].options.map((option,index) =>(
-                                        <FormControlLabel value={option.id} control={<Radio />} label={option.description} />
-                                    
-                                    ))
-                                }
-                                </RadioGroup>  
-                            </FormControl>
-                           
-                        </div>
-                    </CardBody>
-                </Card>
-                <div>
-                    <Button
-                       style={styleButtons}
-                        type="button"
-                        variant="contained"
-                        color="secondary"
-                        size="large"
-                        disabled={preDisable}
-                        onClick={()=>prev()}
-                    >
-                        Prev
+                        </CardHeader>
+                        <CardBody>
+                            <div>
+                                <Typography variant="h6" mt={8}>
+                                    {count}. {questions[selectedIndex].header}
+                                </Typography>
+                                <FormControl component="fieldset">
+                                    <FormLabel component="legend"></FormLabel>
+                                    <RadioGroup aria-label="question" name="question" value={value} onChange={handleChange}>
+                                        {
+                                            questions[selectedIndex].options.map((option, index) => (
+                                                <FormControlLabel key={index} checked={option.id == value} value={option.id} control={<Radio />} label={option.description} />
+
+                                            ))
+                                        }
+                                    </RadioGroup>
+                                </FormControl>
+
+                            </div>
+                        </CardBody>
+                    </Card>
+                    <div className={styles.align_buttons}>
+                        <Button className={styles.button_margin}
+                            type="button"
+                            variant="contained"
+                            color="secondary"
+                            size="large"
+                            disabled={preDisable}
+                            onClick={() => prev()}
+                        >
+                            Prev
                   </Button>
-                  <Button
-                      style={styleButtons}
-                        type="button"
-                        variant="contained"
-                        color="secondary"
-                        size="large"
-                        disabled={nextDisable}
-                        onClick={()=>next()}
-                    >
-                        Next
+                        <Button className={styles.button_margin}
+                            type="button"
+                            variant="contained"
+                            color="secondary"
+                            size="large"
+                            disabled={nextDisable}
+                            onClick={() => next()}
+                        >
+                            Next
                   </Button>
-                  <Button
-                        type="button"
-                        variant="contained"
-                        color="secondary"
-                        size="large"
-                        onClick={()=>submit()}
-                    >
-                        submit
+                        <Button className={styles.button_margin}
+                            type="button"
+                            variant="contained"
+                            color="secondary"
+                            size="large"
+                            onClick={() => submit()}
+                        >
+                            submit
                   </Button>
-                </div>
-                            </GridItem> 
-                            </GridContainer>}
-   </div> )
+                    </div>
+                </GridItem>
+            </GridContainer>}
+        </div>)
 };
-
-
-
-
