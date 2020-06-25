3212d351c04b116c9d86de9cbe89ca1c2cd34091
Herin <herin0506@gmail.com>
2020-06-25 21:13:32 +0530

    Static instruction for Assesment.

--- src/views/CandidateRegisterForAssessment/index.js
+++ src/views/CandidateRegisterForAssessment/index.js
@@ -1,4 +1,4 @@
-import React ,{useState}from "react";
+import React, { Component, useState } from "react";
 // @material-ui/core components
 import { makeStyles } from "@material-ui/core/styles";
 import InputLabel from "@material-ui/core/InputLabel";
@@ -12,7 +12,8 @@ import CardHeader from "components/Card/CardHeader.js";
 import CardAvatar from "components/Card/CardAvatar.js";
 import CardBody from "components/Card/CardBody.js";
 import CardFooter from "components/Card/CardFooter.js";
-import endPoint from '../../variables/app.url'
+import endPoint from '../../variables/app.url';
+import style from './style.module.css';
 
 import avatar from "assets/img/faces/marc.jpg";
 
@@ -37,67 +38,119 @@ const styles = {
 
 const useStyles = makeStyles(styles);
 
+function ShowInstructions(props) {
+  return (
+    <div style={{ margin: "0 20px" }}>
+      <GridContainer>
+        <GridItem>
+          <Card>
+            <CardHeader>
+              <div className={style.display_flex}>
+                <div className={style.float_left, style.w_50}>
+                  TEST DETAILS
+                </div>
+                <div className={style.float_right, style.display_flex}>
+                  <div className={style.float_left, style.header_border}>Total no. of Questions: 10</div>
+                  <div className={style.float_right, style.header_border}>Total Duration: 60mins</div>
+                </div>
+              </div>
+            </CardHeader>
+            <CardBody>
+              <div className={style.font_bold}>
+                THINGS TO REMEMBER
+              </div>
+              <GridContainer>
+                <GridItem className={style.w_100}>
+                  <div >
+                    <ol>
+                      <li>
+                        Before starting the test, please close all chat windows, screen-saver(s), etc. and make sure that you have a stable internet connection.
+                    </li>
+                      <li>
+                        Pressing F5 at any time during the test will cause it to end immediately.
+                    </li>
+                      <li>
+                        If your computer system shuts down suddenly due to your power supply being disconnected, you can resume the test from the same question that you were attempting earlier.
+                        All your previous answers are already saved.
+                    </li>
+                      <li>
+                        Your test will open in a newpopup window once you press the launch test button.
+                    </li>
+                      <li>
+                        When resuming, please start the test as you did before and use the same registration details.
+                    </li>
+                    </ol>
+                    <div className={style.float_right}>
+                      <Button color="primary" onClick={props.onClick}>Proceed</Button>
+                    </div>
+                  </div>
+                </GridItem>
+              </GridContainer>
+            </CardBody>
+          </Card>
+        </GridItem>
+      </GridContainer>
+    </div >
+  );
+}
 
-
-
-
-export default function CandidateRegisterForAssessment() {
+function RegisterForAssessment() {
   const classes = useStyles();
   const [email, setEmail] = useState('pravinmhaske997@gmail.com');
-const [firstName, setfirstName] = useState('pravin');
-const [lastName, setlastName] = useState('mhaske');
-const [countryCode, setcountryCode] = useState('+91');
-const [dob, setdob] = useState('06-sept-1990');
-const [mobile, setMobile] = useState('9623044643');
+  const [firstName, setfirstName] = useState('pravin');
+  const [lastName, setlastName] = useState('mhaske');
+  const [countryCode, setcountryCode] = useState('+91');
+  const [dob, setdob] = useState('06-sept-1990');
+  const [mobile, setMobile] = useState('9623044643');
 
-const handleRegisterCandidate = ()=>{
+  const handleRegisterCandidate = () => {
 
 
-  let reqObj={
-    firstName,
-    "emailAddress":email,
-    lastName,
-    countryCode,
-    "dateOfBirth": dob,
-    "mobileNo": mobile
+    let reqObj = {
+      firstName,
+      "emailAddress": email,
+      lastName,
+      countryCode,
+      "dateOfBirth": dob,
+      "mobileNo": mobile
     }
 
-    console.log("request Object",reqObj);
+    console.log("request Object", reqObj);
     fetch(endPoint.serviceEndPoint, {
-      method:'POST',
-      headers:{
-        'Accept':'application/json',
-        'Content-Type':'application/json',
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
         'Access-Control-Allow-Origin': 'Origin, X-Requested-With, Content-Type, Accept'
       },
       body: JSON.stringify(reqObj)
-    }).then((res)=>res.json())
-      .then((res) =>{
-      
-     console.log("Sucessful invitation ",res)
+    }).then((res) => res.json())
+      .then((res) => {
+
+        console.log("Sucessful invitation ", res)
       })
-  // if(email==='anand@gmail.com' && password==='password'){
-  //   setError(false);
-  //   const url = `${endPoint.serviceEndPoint}valdiateLogin?loginId=`+email+`&password=`+password;
-  //   fetch(url, {
-  //     method:'GET',
-  //     headers:{
-  //       'Accept':'application/json',
-  //       'Content-Type':'application/json',
-  //       'Access-Control-Allow-Origin': 'Origin, X-Requested-With, Content-Type, Accept'
-  //     }
-  //   }).then((res)=>res.json())
-  //     .then(() =>{
-  //       history.push('/admin/invite-candidate')
-  //     })
-  // }else{
-  //   setError(true);
-  //   setHelperText('Incorrect username or password');
-  // }
-}; 
+    // if(email==='anand@gmail.com' && password==='password'){
+    //   setError(false);
+    //   const url = `${endPoint.serviceEndPoint}valdiateLogin?loginId=`+email+`&password=`+password;
+    //   fetch(url, {
+    //     method:'GET',
+    //     headers:{
+    //       'Accept':'application/json',
+    //       'Content-Type':'application/json',
+    //       'Access-Control-Allow-Origin': 'Origin, X-Requested-With, Content-Type, Accept'
+    //     }
+    //   }).then((res)=>res.json())
+    //     .then(() =>{
+    //       history.push('/admin/invite-candidate')
+    //     })
+    // }else{
+    //   setError(true);
+    //   setHelperText('Incorrect username or password');
+    // }
+  };
 
   return (
-    <div style={{margin: "0 20px"}}>
+    <div style={{ margin: "0 20px" }}>
       <GridContainer >
         <GridItem xs={12} sm={12} md={12}>
           <Card>
@@ -106,7 +159,7 @@ const handleRegisterCandidate = ()=>{
               <p className={classes.cardCategoryWhite}>Complete your profile</p>
             </CardHeader>
             <CardBody>
-            
+
               <GridContainer>
                 <GridItem xs={12} sm={12} md={4}>
                   <CustomInput
@@ -116,8 +169,8 @@ const handleRegisterCandidate = ()=>{
                       fullWidth: true
                     }}
 
-                    inputProps={{onChange:(e)=>setfirstName(e.target.value)}}
-                    
+                    inputProps={{ onChange: (e) => setfirstName(e.target.value) }}
+
                   />
                 </GridItem>
                 <GridItem xs={12} sm={12} md={4}>
@@ -127,7 +180,7 @@ const handleRegisterCandidate = ()=>{
                     formControlProps={{
                       fullWidth: true
                     }}
-                    inputProps={{onChange:(e)=>setlastName(e.target.value)}}
+                    inputProps={{ onChange: (e) => setlastName(e.target.value) }}
                   />
                 </GridItem>
                 <GridItem xs={12} sm={12} md={4}>
@@ -137,7 +190,7 @@ const handleRegisterCandidate = ()=>{
                     formControlProps={{
                       fullWidth: true
                     }}
-                    inputProps={{onChange:(e)=>setEmail(e.target.value)}}
+                    inputProps={{ onChange: (e) => setEmail(e.target.value) }}
                   />
                 </GridItem>
               </GridContainer>
@@ -149,7 +202,7 @@ const handleRegisterCandidate = ()=>{
                     formControlProps={{
                       fullWidth: true
                     }}
-                    onChange={(e)=>setMobile(e.target.value)}
+                    onChange={(e) => setMobile(e.target.value)}
                   />
                 </GridItem>
                 <GridItem xs={12} sm={12} md={4}>
@@ -168,15 +221,15 @@ const handleRegisterCandidate = ()=>{
                     formControlProps={{
                       fullWidth: true
                     }}
-                    inputProps={{onChange:(e)=>setdob(e.target.value)}}
+                    inputProps={{ onChange: (e) => setdob(e.target.value) }}
                   />
                 </GridItem>
               </GridContainer>
-            
+
             </CardBody>
             <CardFooter>
-              <Button  color="primary"
-               onClick={handleRegisterCandidate}
+              <Button color="primary"
+                onClick={handleRegisterCandidate}
               >Register Candidate</Button>
             </CardFooter>
           </Card>
@@ -185,3 +238,35 @@ const handleRegisterCandidate = ()=>{
     </div>
   );
 }
+
+class CandidateRegisterForAssessment extends Component {
+  constructor(props) {
+    super(props);
+    this.showInstructions = this.showInstructions.bind(this);
+    this.state = {
+      showInstructions: true
+    };
+  }
+
+  showInstructions() {
+    this.setState({ showInstructions: false });
+  }
+
+  render() {
+    const showInstructions = this.state.showInstructions;
+    if (showInstructions) {
+      console.log("in if")
+      return (
+        <ShowInstructions onClick={this.showInstructions} />
+      );
+    } else {
+      console.log("in else")
+      return (
+        <RegisterForAssessment />
+      );
+    }
+  }
+
+}
+
+export default CandidateRegisterForAssessment;
