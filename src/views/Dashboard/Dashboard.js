ba214361d3cddc3d991ad407a4480296e2a5af0b
kumar abhishek <mail2kumar.2021@gmail.com>
2020-07-10 14:16:48 +0400

    Removing hard coding

--- src/views/Dashboard/Dashboard.js
+++ src/views/Dashboard/Dashboard.js
@@ -2,14 +2,11 @@ import React ,{useEffect} from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Icon from "@material-ui/core/Icon";
 import Store from "@material-ui/icons/Store";
-import Warning from "@material-ui/icons/Warning";
-import DateRange from "@material-ui/icons/DateRange";
 import LocalOffer from "@material-ui/icons/LocalOffer";
 import Update from "@material-ui/icons/Update";
 import Accessibility from "@material-ui/icons/Accessibility";
 import GridItem from "components/Grid/GridItem.js";
 import GridContainer from "components/Grid/GridContainer.js";
-import Danger from "components/Typography/Danger.js";
 import Card from "components/Card/Card.js";
 import CardHeader from "components/Card/CardHeader.js";
 import CardIcon from "components/Card/CardIcon.js";
@@ -23,18 +20,31 @@ const useStyles = makeStyles(styles);
 
 export default function Dashboard() {
   const[subjectCount, setSubjectCount]= React.useState([]);
+
+  // Icon and Color to display on Grid
+  let iconValue = ["warning", "success", "danger", "info", "rose"];
+
+  // Append icon to subjectCount array
+  for(let i=0; i < subjectCount.length; i++) {
+    subjectCount[i].iconColor = random_item(iconValue);
+  }
+
+  function random_item(items) {
+    return items[Math.floor(Math.random()*items.length)];
+  };
+
   const classes = useStyles();
   useEffect(()=>callApi(), []);
 
   const callApi=()=>{
-    fetch(`${endPoint.serviceEndPoint}candidateAssessmentCount`, {
-        method:'GET',
-        headers:{
-          'Accept':'application/json',
-          'Content-Type':'application/json',
-          'Access-Control-Allow-Origin': 'Origin, X-Requested-With, Content-Type, Accept'
-        }
-      }).then((res)=>res.json())
+    fetch(`${endPoint.serviceEndPoint}candidatesAssessments`, {
+      method:'GET',
+      headers:{
+        'Accept':'application/json',
+        'Content-Type':'application/json',
+        'Access-Control-Allow-Origin': 'Origin, X-Requested-With, Content-Type, Accept'
+      }
+    }).then((res)=>res.json())
         .then((res) =>{
           setSubjectCount(res);
         })
@@ -45,99 +55,44 @@ export default function Dashboard() {
   }
 
   return (
-    <div>
-      <GridContainer>
-        <GridItem xs={12} sm={6} md={3}>
-          <Card>
-            <CardHeader color="warning" stats icon>
-              <CardIcon color="warning">
-              <Icon>content_copy</Icon>
-              </CardIcon>
-              <p className={classes.cardCategory}>{subjectCount[2].assessmentName}</p>
-              <h3 className={classes.cardTitle}>{subjectCount[2].candidateCount} <small></small>
-              </h3>
-            </CardHeader>
-            <CardFooter stats>
-              <div className={classes.stats}>
-                <Danger>
-                  <Warning />
-                </Danger>
-                <a href="#pablo" onClick={e => e.preventDefault()}>
-                  {subjectCount[2].assessmentName} Candidate
-                </a>
-              </div>
-            </CardFooter>
-          </Card>
-        </GridItem>
-        <GridItem xs={12} sm={6} md={3}>
-          <Card>
-            <CardHeader color="success" stats icon>
-              <CardIcon color="success">
-                <Store />
-              </CardIcon>
-              <p className={classes.cardCategory}>{subjectCount[1].assessmentName}</p>
-              <h3 className={classes.cardTitle}> {subjectCount[1].candidateCount}</h3>
-            </CardHeader>
-            <CardFooter stats>
-              <div className={classes.stats}>
-                <DateRange />
-                {subjectCount[1].assessmentName} candidate
-              </div>
-            </CardFooter>
-          </Card>
-        </GridItem>
-        <GridItem xs={12} sm={6} md={3}>
-          <Card>
-            <CardHeader color="danger" stats icon>
-              <CardIcon color="danger">
-                <Icon>info_outline</Icon>
-              </CardIcon>
-              <p className={classes.cardCategory}>{subjectCount[3].assessmentName}</p>
-              <h3 className={classes.cardTitle}> {subjectCount[3].candidateCount}</h3>
-            </CardHeader>
-            <CardFooter stats>
-              <div className={classes.stats}>
-                <LocalOffer />
-                {subjectCount[3].assessmentName} candidate
-              </div>
-            </CardFooter>
-          </Card>
-        </GridItem>
-        <GridItem xs={12} sm={6} md={3}>
-          <Card>
-            <CardHeader color="info" stats icon>
-              <CardIcon color="info">
-                <Accessibility />
-              </CardIcon>
-              <p className={classes.cardCategory}>{subjectCount[4].assessmentName}</p>
-              <h3 className={classes.cardTitle}> {subjectCount[4].candidateCount}</h3>
-            </CardHeader>
-            <CardFooter stats>
-              <div className={classes.stats}>
-                <Update />
-               SpringBoot candidate
-              </div>
-            </CardFooter>
-          </Card>
-        </GridItem>
-        <GridItem xs={12} sm={6} md={3}>
-          <Card>
-            <CardHeader color="info" stats icon>
-              <CardIcon color="info">
-                <Accessibility />
-              </CardIcon>
-              <p className={classes.cardCategory}>{subjectCount[5].assessmentName}</p>
-              <h3 className={classes.cardTitle}> {subjectCount[5].candidateCount}</h3>
-            </CardHeader>
-            <CardFooter stats>
-              <div className={classes.stats}>
-                <Update />
-                {subjectCount[5].assessmentName} candidate
-              </div>
-            </CardFooter>
-          </Card>
-        </GridItem>
-       </GridContainer>
-    </div>
+      <div>
+        <GridContainer>
+          {
+            subjectCount.map(item => (
+                <GridItem xs={12} sm={6} md={3}>
+                  <Card>
+                    <CardHeader color={item.iconColor} stats icon>
+                      <CardIcon color={item.iconColor}>
+                        {(() => {
+                          switch (item.iconColor) {
+                            case 'warning':
+                              return  <Icon>content_copy</Icon>;
+                            case 'success':
+                              return <Store />;
+                            case 'danger':
+                              return  <Icon>info_outline</Icon>;
+                            case 'info':
+                              return <Accessibility />;
+                            case 'rose':
+                              return <LocalOffer />
+                            default:
+                              return null;
+                          }
+                        })()}
+                      </CardIcon>
+                      <p className={classes.cardCategory}>{item.assessmentName}</p>
+                      <h3 className={classes.cardTitle}>{item.candidateCount} <small></small>
+                      </h3>
+                    </CardHeader>
+                    <CardFooter stats>
+                      <div className={classes.stats}>
+                        <Update /> {item.assessmentName}
+                      </div>
+                    </CardFooter>
+                  </Card>
+                </GridItem>
+            ))}
+        </GridContainer>
+      </div>
   );
 }
